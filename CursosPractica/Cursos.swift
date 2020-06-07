ddc597bcedd34b977233d6545e45bf2d3b9c1606
friman15889 <friman.vilcapoma@tecsup.edu.pe>
2020-06-07 06:25:12 -0500

    Primer Commit

---
+++ CursosPractica/Cursos.swift
@@ -0,0 +1,15 @@
+//
+//  Cursos.swift
+//  CursosPractica
+//
+//  Created by mbtec22 on 6/7/20.
+//  Copyright © 2020 Tecsup. All rights reserved.
+//
+
+import Foundation
+class Cursos {
+    var nombre = ""
+    var promedioPrac = 0.0
+    var promedioLab = 0.0
+    var examenFinal = 0.0
+}
